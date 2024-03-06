f0b58f527bda12496f8e15be633b31b6e1f11d56
Hasireddy <malikireddy.uma@gmail.com>
2024-03-06 12:58:38 +0100

    login html template rendered

--- admissions/views.py
+++ admissions/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import templates
 
-def say_hello(request):
-    return HttpResponse('Hello world')
+# def say_hello(request):
+#     return HttpResponse('Hello world')
 
 
 def addAdmission(request):
     return HttpResponse("<h1>This is add admission view</h1>")
+
+def say_hello(request):
+    return render(request,'hello.html')
