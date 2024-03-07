a5b71c1e60bcb8551aa7cfb474075e44d066c6c3
Hasireddy <malikireddy.uma@gmail.com>
2024-03-07 14:46:11 +0100

    static folder created

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/',views.registerView),
-    path('login/',views.loginView)
+    path('register/',views.registerView,name="register"),
+    path('login/',views.loginView,name="login"),
+    path('reg/',views.regView,name="reg")
 ]
--- user/views.py
+++ user/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from django.contrib.auth.models import User,auth
+from django.contrib.auth.forms import UserCreationForm
 from . import templates
 
 
 
 def registerView(request):
     if request.method == 'POST':
-        firstname = request.POST['firstname']
-        lastname = request.POST['lastname']
+        
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
         email = request.POST['email']
         password = request.POST['password']
         confirmPassword = request.POST['confirmPassword']
@@ -16,16 +19,29 @@ def registerView(request):
          if User.objects.filter(email==email).exists():
             print('Email already exists')
          else:
-            user = User.objects.create_user(firstname=firstname,lastname=lastname,email=email,password=password,confirmPassword=confirmPassword)
+            user = User.objects.create_user(first_name=first_name,last_name=last_name,email=email,password=password,confirmPassword=confirmPassword)
             user.save()
             return user
-            print('User created')
+        print('User created')
             # return redirect('/')
     else:
      print('User not created')
      return render(request,'registerUser.html')
 
 def loginView(request):
-   return render(request,'loginUser.html')
+   if request.method=='POST':
+      email = request.POST['email']
+      password = request.POST['password'] 
+      user = auth.authenticate(email=email,password=password)
+      if user is not None:
+         auth.login(request,user)
+         return redirect('/')
+      else:
+         messages.info(request,'Invalid credentials')
+         return redirect('login/')
+         
+   else:   
+        return render(request,'loginUser.html')
     
-   
+def regView(request):
+      return render(request,'register.html')
