7d8bc501b36ab4da14128ed7a29d0b3460787dc0
Hasireddy <malikireddy.uma@gmail.com>
2024-03-07 09:12:59 +0100

    model created and migrated

--- user/models.py
+++ user/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+class Register(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.TextField()
+    confirmPassword = models.TextField()
